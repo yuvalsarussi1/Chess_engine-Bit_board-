a8a3d1245757cf205ac3275840f427a68e586399
yuval <yuvalsarussi12@gmail.com>
2025-08-25 21:10:29 +0300

    ADD ATTACKS def for pieces

--- moves.py
+++ moves.py
@@ -1,20 +1,32 @@
+from bitboard import *
+from magic import *
 #condition_1 = ligal piece move
 
 
-def Pawn_attack():
-    pass
+def Pawn_attacks_eat(square_num: int) -> int:
+    return PAWN_MASK_EAT[square_num]
+def Pawn_attacks_walk(square_num: int) -> int:
+    return PAWN_MASK_WALK[square_num]
+def Knight_attacks(square_num: int) -> int:
+   return KNIGHT_MASK[square_num]
+def King_attack(square_num: int) -> int:
+    return KING_MASK[square_num]
 
-def Knight_attack():
-    pass
 
-def King_attack():
-    pass
 
-def Rook_attack():
-    pass
-
-def Bishop_attack():
-    pass
+def Rook_attack(square_num: int) -> int:
+    occ = ALL_OCCUPANCY & ROOK_EXCLUDE_EDGES[square_num]
+    row, col = divmod(square_num, 8)
+    attack_mask = (occ * MAGIC_NUMBER_ROOK[square_num]) >> (64 - ROOK_RELEVANT_BITS[row][col])
+    return ROOK_ATTACKS[square_num][attack_mask]
+    
+def Bishop_attack(square_num: int) -> int:
+    occ = ALL_OCCUPANCY & BISHOP_EXCLUDE_EDGES[square_num]
+    row, col = divmod(square_num, 8)
+    attack_mask = (occ * MAGIC_NUMBER_BISHOP[square_num]) >> (64 - BISHOP_RELEVANT_BITS[row][col])
+    return BISHOP_ATTACKS[square_num][attack_mask]
+    
+    
 
 def Queen_attack():
     pass
